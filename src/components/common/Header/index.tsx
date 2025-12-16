25a4aa7fccba5e2c55fd8ef3579b50291fdec8dd
dawoonkim <hec8897@naver.com>
2025-12-16 23:02:31 +0900

    feat: Header 및 FlexContainer 컴포넌트 추가
    - Header 컴포넌트를 styled-components로 리팩토링하고 FlexContainer를 추가하여 유연한 레이아웃 구성 가능
    - FlexContainer는 다양한 flexbox 속성을 지원하며, 재사용 가능한 UI 컴포넌트로 설계됨
    - 관련 타입 및 유틸리티 함수도 함께 정의하여 코드의 일관성 및 가독성 향상

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,9 +1,21 @@
 "use client";
 import styled from "@emotion/styled";
+import { FlexContainer } from "@/components/ui";
 
-export const Header = styled.header`
+const HeaderContainer = styled.header`
   height: 64px;
+  padding: 0 20px;
   box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
 `;
 
-export default Header;
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <FlexContainer justify="space-between" align="center" fullHeight>
+        <div>1</div>
+        <h1>Dividend Logbook</h1>
+        <div>1</div>
+      </FlexContainer>
+    </HeaderContainer>
+  );
+}
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,6 @@
 // 기본 UI 컴포넌트 exports
+export { default as FlexContainer } from "./FlexContainer";
+export type { FlexContainerProps } from "./FlexContainer/types";
+
 // 예: export { default as Button } from "./Button";
 // export { default as Input } from "./Input";
